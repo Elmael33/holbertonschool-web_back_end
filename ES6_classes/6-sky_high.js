b70f2137554232ef13fb54a5c7c6b71050eee68a
Elmael33 <mael.salet33@gmail.com>
2024-07-22 01:11:50 +0200

    [fix] 6-sky_high; change syntax for checker

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,7 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
     this._sqft = sqft;
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
@@ -12,18 +12,19 @@ export default class SkyHighBuilding extends Building {
       throw new TypeError('floors must be a number');
     }
   }
+
   get sqft() {
     return this._sqft;
   }
 
   set sqft(sqft) {
     this._sqft = sqft;
-
   }
+
   get floors() {
     return this._floors;
   }
-  
+
   set floors(floors) {
     this._floors = floors;
   }
