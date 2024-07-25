3e44440479010e7b0a3210c743cdf8966edf818e
Elmael33 <mael.salet33@gmail.com>
2024-07-25 14:36:28 +0200

    [add] 6-final_user.js; change syntax 2

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const results = await Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
 
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+    return results.map((result) => ({ status: 'fulfilled', value: result.value }));
+  } catch (error) {
+    console.error('Error handling profile signup:', error);
+    return [];
+  }
 }
