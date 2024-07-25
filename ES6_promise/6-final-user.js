c3dbc91ef13ec66726516a484eee2cb61199f4bd
Elmael33 <mael.salet33@gmail.com>
2024-07-25 16:33:49 +0200

    [add] 6-final_user.js; fix npx eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const results = await Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]);
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 
-    return results.map((result) => ({ status: 'fulfilled', value: result.value }));
-  } catch (error) {
-    console.error('Error handling profile signup:', error);
-    return [];
-  }
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+  }));
 }
