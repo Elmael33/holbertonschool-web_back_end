f78f09c8dbd6c3c3c104b44133947743f762bfb7
Elmael33 <mael.salet33@gmail.com>
2024-08-16 23:15:19 +0200

    [fix] 1-concurent.py/ 4-task.py; change the return syntax

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,13 +2,15 @@
 """Module to run multiple asyncio Tasks\
     concurrently and collect their results."""
 
-from typing import List
 import asyncio
+import random
+from typing import List
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Return a list of delays in the order they were completed."""
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for i in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
