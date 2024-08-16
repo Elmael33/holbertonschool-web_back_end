950a81b8af3338006a0fff12d70f0b3ffc4eaa84
Elmael33 <mael.salet33@gmail.com>
2024-08-16 23:04:07 +0200

    [fix] 1-concurent.py/ 4-task.py; change the return syntax

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,6 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a list of delays in the order they were completed."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,7 +8,6 @@ import asyncio
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Return a list of delays in the order they were completed."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
