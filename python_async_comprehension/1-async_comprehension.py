d279ab4b76610bb0bad8e0e02403c2b50923ee39
Elmael33 <mael.salet33@gmail.com>
2024-09-06 10:04:06 +0200

    [fix] 2-measure_runtime.py; syntax

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """module defines a coroutine that collects\
     random numbers using async comprehension."""
-    
-from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension():
     """Collect 10 random numbers using an async\
         comprehending over async_generator."""
     return [number async for number in async_generator()]
