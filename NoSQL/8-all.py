e54e2d00b643271641803f18c2710a9306839ceb
Elmael33 <mael.salet33@gmail.com>
2024-12-15 19:17:39 +0100

    [fix] 8-all.py; remove error

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
-""" Function to list all documents in a collection """
+"""Function to list all documents in a collection"""
 
 
 def list_all(mongo_collection):
     if mongo_collection is None:
         return []
 
-    try:
-        documents = list(mongo_collection.find())
-    except Exception as e:
-        print(f"Error occurred while retrieving documents: {e}")
-        return []
+    documents = list(mongo_collection.find())
 
     return documents
