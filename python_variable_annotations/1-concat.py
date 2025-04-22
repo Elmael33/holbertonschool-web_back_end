e1bf4da9122c94935fee1b7f3a1327d5da4cf26c
Elmael33 <158865137+Elmael33@users.noreply.github.com>
2025-04-22 09:19:53 +0200

    Delete python_variable_annotations directory

--- python_variable_annotations/1-concat.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-"""Module with annotations concat that takes two string."""
-
-
-def concat(str1: str, str2: str) -> str:
-    """Returns sum of two strings."""
-    return str1 + str2
