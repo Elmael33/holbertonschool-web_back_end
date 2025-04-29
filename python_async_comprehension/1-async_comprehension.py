f26562c16863756e1a10830f3d7216200b26b7d8
Elmael33 <mael.salet33@gmail.com>
2025-04-29 20:44:54 +0200

    [fix] implement missing List in 1-async_comprehension and remove 2 newline in the code to optimise

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Module defines an asynchronous function
     that gathers values from an async generator."""
-
+import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Use an async comprehension to gather and
         return 10 random numbers from the async generator."""
     return [number async for number in async_generator()]
