8b7e6cb552043440f674f65645c0d47b0339385a
Elmael33 <mael.salet33@gmail.com>
2025-04-29 20:55:05 +0200

    [fix] Return back previous code with AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,12 +3,12 @@
     that yields random numbers after delays."""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Asynchronously yield 10 random floats between 0 and 10,
         with a 1-second pause between each."""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
